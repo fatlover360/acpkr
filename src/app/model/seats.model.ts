79b343523e2fdadb23286894cceddfb228ebbcdf
João Vieira <joaocsvieira92@gmail.com>
2018-07-25 18:59:01 +0100

    tables restructoring

--- src/app/model/seats.model.ts
+++ src/app/model/seats.model.ts
@@ -4,12 +4,20 @@ export class Seats {
   cardOne: string;
   cardTwo: string;
   chips: number;
+  small: boolean;
+  big: boolean;
+  button: boolean;
 
-  constructor(number: number, user: string, cardOne: string, cardTwo: string, chips: number) {
+  constructor(number: number, user: string, cardOne: string, cardTwo: string, chips: number, small: boolean,
+              big: boolean,
+              button: boolean) {
     this.number = number;
     this.user = user;
     this.cardOne = cardOne;
     this.cardTwo = cardTwo;
     this.chips = chips;
+    this.small = small;
+    this.big = big;
+    this.button = button;
   }
 }
